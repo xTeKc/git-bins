309ff91f48898ac812269be8e0821ede8aa6d990
xTeKc <81730792+xTeKc@users.noreply.github.com>
2023-07-03 02:08:46 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-# Copyright (c) xTekC and git-binstall contributors.
+# Copyright (c) xTekC and git-bins contributors.
 # Licensed under MPL-2.0. See LICENSE for details.
-# https://github.com/xTeKc/git-binstall/blob/main/LICENSE
+# https://github.com/xTeKc/git-bins/blob/main/LICENSE
